26fc92d76b06341f0db8fb62a070bd46d6e592ab
Simon Schmidt <simon.schmidt@ges.thm.de>
2017-08-08 11:45:40 +0200

    Print

--- CardgameCollection/Program.cs
+++ CardgameCollection/Program.cs
@@ -17,6 +17,7 @@ namespace CardgameCollection
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.Write("started");
         }
     }
 }
